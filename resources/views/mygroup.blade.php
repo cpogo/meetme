bbdefbb96fcc413d7dc952cb25a3972feff0b02f
metacris93 <metacris93@gmail.com>
2015-11-11 23:10:44 -0500

    disenie como se van a visualizar los grupos de  mygroup.blade.php

--- resources/views/mygroup.blade.php
+++ resources/views/mygroup.blade.php
@@ -351,27 +351,60 @@
           </h1>
           <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li class="active">Calendar</li>
+            <li class="active">Group</li>
           </ol>
         </section>
 
         <!-- Main content -->
-        <section class="content">
-          <div class="row">
-            <div class="col-md-3">
+          <section class="content">
+            <div class="box">
               <div class="box box-primary">
-                <div class="box-header">
-                  <h3 class="box-title">Manage your Group</h3>
-                </div>
-                <div class="box-body">
-
-
-                </div><!-- /.box-body -->
-              </div>
-            </div><!-- /.col -->
-
-          </div>
-        </section>
+                  <div class="box-header with-border">
+                      <h3 class="box-title">Group name</h3>
+                  </div>
+                  <div class="box-body">
+                      <div class="box-header with-border">
+                        <div class="thumbnail">
+                          <img src="{{asset('images/avatar5.png')}}" class="img-circle img-responsive" alt="owner" width="140" height="140">
+                          <div class="caption">
+                              <h3 style='text-align: center;'>Owner</h3>
+                              <h4 style='text-align: center;'>Owner name</h4>
+                          </div>
+                        </div>  
+                      </div>
+                      <div class="row">
+                        <div class="col-sm-6 col-md-4">
+                          <div class="thumbnail">
+                            <img src="{{asset('images/avatar3.png')}}" class="img-circle img-responsive" alt="owner" width="140" height="140">
+                            <div class="caption">
+                                <h3 style='text-align: center;'>Member</h3>
+                                <h4 style='text-align: center;'>Member</h4>
+                            </div>
+                          </div>  
+                        </div>
+                        <div class="col-sm-6 col-md-4">
+                          <div class="thumbnail">
+                            <img src="{{asset('images/avatar2.png')}}" class="img-circle img-responsive" alt="owner" width="140" height="140">
+                            <div class="caption">
+                                <h3 style='text-align: center;'>Member</h3>
+                                <h4 style='text-align: center;'>Member</h4>
+                            </div>
+                          </div>  
+                        </div>
+                        <div class="col-sm-6 col-md-4">
+                          <div class="thumbnail">
+                            <img src="{{asset('images/avatar04.png')}}" class="img-circle img-responsive" alt="owner" width="140" height="140">
+                            <div class="caption">
+                                <h3 style='text-align: center;'>Member</h3>
+                                <h4 style='text-align: center;'>Member</h4>
+                            </div>
+                          </div>  
+                        </div>  
+                      </div>                                        
+                  </div><!-- /.box-body -->
+              </div>            
+            </div>
+        </section>              
       </div>
 
       <footer class="main-footer">
