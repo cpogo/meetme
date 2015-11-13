e735dc9331051149eab99f314ead0e01ca74e4fe
cpogo <cpogo@fiec.espol.edu.ec>
2015-11-13 08:13:14 -0500

    model: User envía false, no -1 ya que eso se considera true view: la busqueda presenta enlaces a usuarios

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -86,7 +86,9 @@
 								<ul class="nav nav-tabs">
 									<li class="active"><a href="#activity" data-toggle="tab">Activity</a></li>
 {{--									<li><a href="#timeline" data-toggle="tab">Timeline</a></li>--}}
+@if ($user->id == $profile->id)
 									<li><a href="#settings" data-toggle="tab">Settings</a></li>
+@endif
 								</ul>
 								<div class="tab-content">
 									<div class="active tab-pane" id="activity">
@@ -271,7 +273,7 @@
 										</ul>
 									</div><!-- /.tab-pane -->
 --}}
-
+@if ($user->id == $profile->id)
 									<div class="tab-pane" id="settings">
 										<form class="form-horizontal" action="{{ url('profile_settings') }}" method="post">
 											{{ csrf_field() }}
@@ -331,6 +333,7 @@
 											</div>
 										</form>
 									</div><!-- /.tab-pane -->
+@endif
 								</div><!-- /.tab-content -->
 							</div><!-- /.nav-tabs-custom -->
 						</div><!-- /.col -->
@@ -340,14 +343,5 @@
 			</div><!-- /.content-wrapper -->
 @endsection
 @section('scripts')
-		<!-- jQuery 2.1.4 -->
-		<script src="{{ asset('plugins/jQuery/jQuery-2.1.4.min.js') }}"></script>
-		<!-- Bootstrap 3.3.5 -->
-		<script src="{{ asset('js/bootstrap.min.js') }}"></script>
-		<!-- FastClick -->
-		<script src="{{ asset('plugins/fastclick/fastclick.min.js') }}"></script>
-		<!-- AdminLTE App -->
-		<script src="{{ asset('js/app.min.js') }}"></script>
-		<!-- AdminLTE for demo purposes -->
-		<script src="{{ asset('js/demo.js') }}"></script>
+		@include('app.scripts_page_')
 @endsection
